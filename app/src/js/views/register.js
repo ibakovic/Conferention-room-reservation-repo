c2e26a330583be66b1a6fd5c43cc30b68df59c0e
ibakovic <ivbakovi@fesb.hr>
2016-02-10 16:22:20 +0100

    E-mail format check
    - on frontend and backend
    - new input element added in register template

--- app/src/js/views/register.js
+++ app/src/js/views/register.js
@@ -13,7 +13,8 @@ var RegisterView = Marionette.ItemView.extend({
 		firstName: '#registerFirstName',
 		lastName: '#registerLastName',
 		username: '#registerUsername',
-		password: '#registerPassword'
+		password: '#registerPassword',
+		email: '#registerEmail'
 	},
 
 	events: {
@@ -27,8 +28,39 @@ var RegisterView = Marionette.ItemView.extend({
 		var lastName = this.ui.lastName.val().trim();
 		var username = this.ui.username.val().trim();
 		var password = this.ui.password.val().trim();
+		var email = this.ui.email.val().trim();
 		
 		//provjera upisanog!!!
+		if(!firstName) {
+			alert('First name required!');
+			return;
+		}
+
+		if(!lastName) {
+			alert('Last name required!');
+			return;
+		}
+
+		if(!username) {
+			alert('Username required!');
+			return;
+		}
+
+		if(!password) {
+			alert('Password required!');
+			return;
+		}
+
+		if(!email) {
+			alert('E-mail required!');
+			return;
+		}
+
+		var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+		if(!pattern.test(email)) {
+			alert('Wrong e-mail format!');
+			return;
+		}
 
 		popsicle.request({
 			method: 'POST',
@@ -37,7 +69,8 @@ var RegisterView = Marionette.ItemView.extend({
 				firstName: firstName,
 				lastName: lastName,
 				username: username,
-				password: password
+				password: password,
+				email: email
 			}
 		})
 		.then(function registerComplete(res) {
